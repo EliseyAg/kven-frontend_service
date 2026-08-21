8ce2c8709bd030345a3db21b55c87aae077af4b7
EliseyAg <elisey.tagan@gmail.com>
2026-08-21 14:34:40 +0300

    Minor fix in app.py

--- frontend_service/app.py
+++ frontend_service/app.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from config import Config
 from utils.api_client import APIClient
-from utils.session import SessionManager
+#from utils.session import SessionManager
 
 
 app = Flask(__name__)
@@ -29,7 +29,6 @@ def login():
             return redirect(url_for('feed'))
         return render_template('auth/login.html')
 
-        # POST: обработка логина
     username_or_email = request.form.get('username_or_email')
     password = request.form.get('password')
 
@@ -37,12 +36,10 @@ def login():
         flash('Please fill all fields', 'error')
         return render_template('auth/login.html'), 400
 
-    # Вызываем Auth Service через API клиент
     try:
-        response = api_client.login(username_or_email, password)
+        response = APIClient.login(username_or_email, password)
 
         if response.get('access_token'):
-            # Сохраняем токены в сессии
             session['access_token'] = response['access_token']
             session['refresh_token'] = response['refresh_token']
             session['user'] = response['user']
